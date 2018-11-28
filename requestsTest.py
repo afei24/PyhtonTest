164e187b20dd1baeb487e168e1b46ec7a111dbba
ruby <137724742@qq.com>
2018-11-28 22:07:51 +0800

    requests库高级用法

--- requestsTest.py
+++ requestsTest.py
@@ -3,4 +3,21 @@
 # author:afei  time:2018/11/27
 
 import requests
+from  requests_oauthlib import  OAuth1
 
+'''
+proxies = {
+    'http': 'socks5://18363001105lu:lpf137724742@10.10.1.10:3128',
+    'https': 'socks5://18363001105lu:lpf137724742@10.10.1.10:3128',
+}
+
+response = requests.get("https://www.taobao.com", proxies)
+print(response.text)
+
+'''
+
+
+url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+auth = OAuth1('key', 'secret', 'token', 'token_secret')
+response = requests.get(url, auth=auth)
+print(response.status_code)
